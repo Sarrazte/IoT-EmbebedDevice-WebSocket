785f753b4c4e2f82dd020609b3f7a261d3e6857e
Sarrazte <sebastianarrazate99@gmail.com>
2022-04-12 14:59:01 -0500

    Eliminando archivo de Python inutil y agregando manejo de subprocesos para publicar y suscribir con python. Agregando libreria Adafruit_BBIO para manejo de GPIO de BBB

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,85 +1,81 @@
-#
-# Copyright 2021 HiveMQ GmbH
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import time
 import paho.mqtt.client as paho
 from paho import mqtt
 import random
 import time
 import json
+import threading
+import Adafruit_BBIO.GPIO as GPIO
 
+#Indicamos el PIN de salida para el LED
+outPin  = "P9_12"
+#Definimos el PIN "P9_12" como salida
+GPIO.setup(outPin, GPIO.OUT)
+
+#Inicializamos valores para la simulacion de los sensores
 velocidades = 0
-long = -103.337436
+long = -103.337436 
 latid = 20.683226
-movimiento = 0.000700
-i = 1
+movimiento = 0.000700 
 
-
-# setting callbacks for different events to see if it works, print the message etc.
-def on_connect(client, userdata, flags, rc, properties=None):
+#Funcion que se ejecuta cuando se logra la conexion
+def on_connect(client, userdata, flags, rc, properties=None): #Funcion ejecutada por paho cuando se conecte al Broker
     print("CONNACK received with code %s." % rc)
+    client.subscribe("IoT/Led", qos=1)
+    print('Hilo: ', threading.current_thread().ident)
 
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
+#Funcion que se ejecuta cuando se recibe un mensaje del Broker
+def on_message(client, userdata, msg): #Funcion ejecutada cuando llega un mensaje al Broker
+    if msg.payload == b'ON':
+        encenderLed()
+    else:
+        apagarLed()
 
-# print which topic was subscribed to
-#def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-#    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+#Funcion que se ejecuta cuando se consigue la conexion al broker MQTT
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Suscripcion Exitosa")
 
-# print message, useful for checking if it was successful
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
-client = paho.Client(client_id="sebastian", userdata=None, protocol=paho.MQTTv5)
+#Se crea la instancia del cliente MQTT y se mencionan las funciones que se ejecutaran al utilizar metodos del cliente
+client = paho.Client(client_id="sebastian", userdata=None, protocol=paho.MQTTv5) #Instancia del cliente MQTT
 client.on_connect = on_connect
-
-# enable TLS for secure connection
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-client.username_pw_set("sarrazate", "Last990702")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("9a3d19b43038457cab037734cb0f9e35.s1.eu.hivemq.cloud", 8883)
-
-# setting callbacks, use separate functions like above for better visibility
-#client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.on_publish = on_publish
+client.on_subscribe = on_subscribe
 
-# subscribe to all topics of encyclopedia by using the wildcard "#"
-#client.subscribe("testTopic", qos=1)
-while i > 0:
+#Funcion inicializada por el subproceso "hiloConexion"
+def conexion():
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS) # enable TLS for secure connection
+    client.username_pw_set("sarrazate", "Last990702") # set username and password
+    client.connect("9a3d19b43038457cab037734cb0f9e35.s1.eu.hivemq.cloud", 8883) # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+
+#Creamos hilo manejador de conexion y suscripcion
+hiloConexion = threading.Thread(name="hiloConexion", target=conexion)
+hiloConexion.start()
+
+def publicarDatos(coordenadasToJSON, velocidades):
+    #Publicamos los mensajes
+    client.publish("IoT/Velocidad", payload=velocidades, qos=1)
+    client.publish("IoT/Coord", payload=coordenadasToJSON, qos=1)
+    print(coordenadasToJSON)
+    time.sleep(3)
+
+def encenderLed():
+    GPIO.output(outPin, GPIO.HIGH)
+    print("LED ENCENDIDO")
+
+def apagarLed():
+    GPIO.output(outPin, GPIO.LOW)
+    GPIO.cleanup()
+    print("LED APAGADO")
+
+while True:
     velocidades = random.randrange(0,200,1)
     long = long - movimiento
     latid = latid + movimiento
     coordenadasNuevas = {'lat':latid, 'lng':long}
     coordenadasToJSON = json.dumps(coordenadasNuevas)
-    print(coordenadasToJSON)
-# a single publish, this can also be done in loops, etc.
-    client.publish("IoT/Velocidad", payload=velocidades, qos=1)
-    time.sleep(1)
-    #client.publish("IoT/Long", payload=long, qos=1)
-    #client.publish("IoT/Lat", payload=latid, qos=1)
-    client.publish("IoT/Coord", payload=coordenadasToJSON, qos=1)
-    time.sleep(3)
-    i += 1
-
-
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
-#client.loop_forever()
+    publicarDatos(coordenadasToJSON, velocidades)
+    #Creamos hilo manejador de publicaciones
+    #hiloPublicador = threading.Thread(name="hiloPublicador", target=publicarDatos, args=(coordenadasToJSON, velocidades))
+    #hiloPublicador.start()
+    #hiloPublicador.join()
+    client.loop()
