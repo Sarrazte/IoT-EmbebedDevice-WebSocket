cfc868e1ecb50ca589b073b48cfb802a19dfcbdd
Sarrazte <sebastianarrazate99@gmail.com>
2022-04-28 11:50:44 -0500

    Actualización de Publicador mqtt_client.py agregando función de lectura de temperaturas con sensor LM35

--- mqtt_client.py
+++ mqtt_client.py
@@ -5,12 +5,15 @@ import random
 import time
 import json
 import threading
-#import Adafruit_BBIO.GPIO as GPIO
+#import Adafruit_BBIO.GPIO as GPIO // Descomentar para ejecutar en BBB
+#import Adafruit_BBIO.ADC as ADC // Descomentar para ejecutar en BBB
 
 #Indicamos el PIN de salida para el LED
-outPin  = "P9_12"
+#outPin  = "P9_12" // Descomentar para ejecutar en BBB
 #Definimos el PIN "P9_12" como salida
-#GPIO.setup(outPin, GPIO.OUT)
+#GPIO.setup(outPin, GPIO.OUT) // Descomentar para ejecutar en BBB
+#Llamamos setup para iniciar ADC
+#ADC.setup() // Descomentar para ejecutar en BBB
 
 #Inicializamos valores para la simulacion de los sensores
 velocidades = 0
@@ -19,6 +22,7 @@ long = -103.337436
 latid = 20.683226
 movimiento_arriba = -0.000700
 movimiento_derecha = 0.001250
+temperatura = 0
 
 
 #Funcion que se ejecuta cuando se logra la conexion
@@ -54,22 +58,30 @@ def conexion():
 hiloConexion = threading.Thread(name="hiloConexion", target=conexion)
 hiloConexion.start()
 
-def publicarDatos(coordenadasToJSON, velocidades, gasolina):
+def publicarDatos(coordenadasToJSON, velocidades, gasolina, temperatura):
     #Publicamos los mensajes
     client.publish("IoT/Velocidad", payload=velocidades, qos=1)
     client.publish("IoT/Coord", payload=coordenadasToJSON, qos=1)
     client.publish("IoT/Gasolina", payload=gasolina, qos=1)
+    client.publish("IoT/Temp", payload=temperatura, qos=1)
     print(coordenadasToJSON)
     time.sleep(3)
 
 def encenderLed():
-    #GPIO.output(outPin, GPIO.HIGH)
+    #GPIO.output(outPin, GPIO.HIGH) // Descomentar para ejecutar en BBB
     print("LED ENCENDIDO")
 
 def apagarLed():
-    #GPIO.output(outPin, GPIO.LOW)
-    #GPIO.cleanup()
+    #GPIO.output(outPin, GPIO.LOW) // Descomentar para ejecutar en BBB
+    #GPIO.cleanup() // Descomentar para ejecutar en BBB
     print("LED APAGADO")
+
+def lecturaTemperatura():
+    #reading = ADC.read('P9_40') // Descomentar para ejecutar en BBB
+    #millivolts = reading * 1800 // Descomentar para ejecutar en BBB
+    #temp_c = millivolts / 10 // Descomentar para ejecutar en BBB
+    #print('Temperatura: '+str(temp_c)+' °C') // Descomentar para ejecutar en BBB
+    #return temp_c // Descomentar para ejecutar en BBB
 
 while True:
     velocidades = random.randrange(0,200,1)
@@ -77,7 +89,8 @@ while True:
     latid = latid + movimiento_derecha
     coordenadasNuevas = {'lat':latid, 'lng':long}
     coordenadasToJSON = json.dumps(coordenadasNuevas)
-    publicarDatos(coordenadasToJSON, velocidades, gasolina)
+    temperatura = lecturaTemperatura() 
+    publicarDatos(coordenadasToJSON, velocidades, gasolina, temperatura)
     #Creamos hilo manejador de publicaciones
     #hiloPublicador = threading.Thread(name="hiloPublicador", target=publicarDatos, args=(coordenadasToJSON, velocidades))
     #hiloPublicador.start()
